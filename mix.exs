3796c1caaa397532e39649d6df259aa8dad87352
Avidor Turkewitz <aviturkewitz@gmail.com>
2018-11-23 11:55:28 -0500

    setup database connection

---
+++ mix.exs
@@ -0,0 +1,26 @@
+defmodule EctoLock.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :ecto_lock,
+      version: "0.1.0",
+      elixir: "~> 1.7",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:ecto_sql, "~> 3.0"},
+      {:postgrex, ">= 0.0.0"}
+    ]
+  end
+end
