4a062f497b48706a0f7380ab21987ea9508a824f
Avidor Turkewitz <aviturkewitz@gmail.com>
2019-01-07 10:51:16 -0500

    add stock invoice functions

--- lib/ecto_lock/bill_pending_invoices.ex
+++ lib/ecto_lock/bill_pending_invoices.ex
@@ -8,18 +8,25 @@ defmodule EctoLock.BillPendingInvoices do
   end
 
   def bill_pending_invoice(invoice_id) do
-
+    invoice = get_invoice(invoice_id)
+    bill_through_api(invoice)
+    mark_invoice_sent(invoice)
   end
 
   def get_invoice(id) do
-
+    Repo.get(Invoice, id)
   end
 
   def bill_through_api(invoice) do
-
+    # let's assume it takes a second to hit the API
+    IO.puts("Sending invoice #{invoice.id}...")
+    :timer.sleep(1000)
+    IO.puts("Invoice #{invoice.id} sent!")
   end
 
-  def update_invoice(invoice) do
-
+  def mark_invoice_sent(invoice) do
+    invoice
+    |> Invoice.changeset(%{pending: false})
+    |> Repo.update()
   end
 end
